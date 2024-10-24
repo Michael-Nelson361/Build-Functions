a8c9bd17416c93a8ef0623a549ed2521e71a4a1d
Michael Nelson <zsc.michaeln@gmail.com>
2024-10-24 12:04:48 -0500

    Created main.py for main program Added conditions for running as main program or as an import

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
-# Pull in base imports for usage
+"""
+Program to build files holding functions for usage in other Python applications.
+"""
 
-# Establish for the user what the program is about
-
-# Import the newly created files
-
-# Provide a menu
-
+import argparse
 import os
 
-manage_library()
+current_path = os.getcwd()    # Get and store current working directory
+
+def test_pass(message:="current test",verbose=False):
+    """
+    Function prints a message if it is executed. Purely for testing and debugging.
+
+    Parameters:
+    -----------
+    - message: (str) Message to print. Default is 'current test'.
+    - verbose: (bool) Determines whether or not to print a message. Default value is False.
+
+    Returns:
+    --------
+    None
+    """
+
+    try:
+        if verbose == True:
+            final_print = "Test condition: " + str(message)
+    except:
+        print("Invalid value passed to function. Exiting.")
+
+def main(verbose=False):
+    pass
+
+if __name__ == "__main__":
+    main()
